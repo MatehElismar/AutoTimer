597cbcf19c9acb38396acc44d7430d78b8cbaacb
mateh elismar <matematicoelismar@gmail.com>
2019-09-15 07:25:50 -0400

    New Projects added

--- args_handler.py
+++ args_handler.py
@@ -15,10 +15,11 @@ class ArgsHandler():
             usage='''autotimer <command> [<args>]
 
                 The most commonly used autotimer commands are:
-                new        Create a New Project
-                start      Start Tracking an Existent Project 
-                delete     Delete A project and its history
-                list       Show all existent projects
+                new             Create a New Project
+                start           Start Tracking an Existent Project 
+                delete          Delete A project and its history
+                list            Show all existent projects
+                list_running    Show Current Running Projects
                 ''')
 
         parser.add_argument('command', help='Subcommand to run')
@@ -26,9 +27,8 @@ class ArgsHandler():
         # parse_args defaults to [1:] for args, but you need to
         # exclude the rest of the args too, or validation will fail
         args = parser.parse_args(sys.argv[1:2]) 
-        
         if not hasattr(self, args.command):
-            print( 'Unrecognized command')
+            print( 'Unrecognized command' )
             parser.print_help()
             exit(1)
         # use dispatch pattern to invoke method with same name
